9a1eaa6dbaa300953e61c15e7132a1673eede4bd
victorclee <victorchristopherlee@gmail.com>
2017-09-04 16:38:41 -0500

    Created container and box. Going to try TweenLite.

--- app/assets/javascripts/rectangles.js
+++ app/assets/javascripts/rectangles.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+Draggable.create("#box", {
+  type:"x,y",
+  bounds:"#container",
+  overshootTolerance:0,
+  throwProps:true
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # Since this is a single page JavaScript app, I broke the RESTful routing guidelines of "get-index", "get-new", "post-create", "get-show", "get-edit", "put-update", "delete-destroy" and used "home" instead, made more sense this way.
 
   get '/' => 'rectangles#home'
 
